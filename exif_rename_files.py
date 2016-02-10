6aa633f8713ba603aa2bf1d098fb2d87b4d0e06b
Miguel Tremblay <gitlab.miguel@ptaff.ca>
2016-02-10 11:16:29 +0100

    Dead code removed

--- exif_rename_files.py
+++ exif_rename_files.py
@@ -245,45 +245,6 @@ def get_unique_path_for_images(dNewPathRawWithPossibleCollision):
          my_print ("----")
 
    return dNewPathUnique
-
-def ignore_files(dir, files):
-   """
-   Function to be called by shutil.copytree to only copy the directory, not the files.
-   Recipe comes from here: http://ur1.ca/ogemo
-   """
-   return [f for f in files if os.path.isfile(os.path.join(dir, f))]
-
-def copytree(src, dst, symlinks = False, ignore = None):
-   """
-   Local copytree that permits to copy a directory to another one, even if the source already exists.
-   Source of the function is here: http://ur1.ca/ogen8
-   """
-
-   if not os.path.exists(dst):
-      os.makedirs(dst)
-      shutil.copystat(src, dst)
-   lst = os.listdir(src)
-   if ignore:
-      excl = ignore(src, lst)
-      lst = [x for x in lst if x not in excl]
-   for item in lst:
-      s = os.path.join(src, item)
-      d = os.path.join(dst, item)
-      if symlinks and os.path.islink(s):
-         if os.path.lexists(d):
-            os.remove(d)
-         os.symlink(os.readlink(s), d)
-         try:
-            st = os.lstat(s)
-            mode = stat.S_IMODE(st.st_mode)
-            os.lchmod(d, mode)
-         except:
-            pass # lchmod not available
-      elif os.path.isdir(s):
-         copytree(s, d, symlinks, ignore)
-      else:
-         shutil.copy2(s, d)
-
       
 def write_log(sPath):
    """
@@ -348,7 +309,6 @@ def exif_rename_files(lInputDirectory, sOuputDirectory=None, bRecursiveInput=Fal
 	 if not os.path.exists(sDirectory):
 	    os.makedirs(sDirectory)
 	 
-#     copytree(dNewPathUnique.values(), sOuputDirectory, ignore=ignore_files)
 
    # Duplicate files
    duplicate_images(dNewPathUnique)
